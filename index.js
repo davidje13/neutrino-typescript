66ac33a5ab95ecf8ae5db89e898495a9c8d2e7ab
David Evans <davidje13@users.noreply.github.com>
2019-07-13 00:44:10 +0100

    Only add plugins/presets if not already specified

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
 const applyNeutrinoPatches = require('neutrino-patch');
 
+function hasEntryContaining(list, check) {
+  const c = Array.isArray(check) ? check[0] : check;
+  return list
+    .map((v) => Array.isArray(v) ? v[0] : v)
+    .some((x) => x.includes(c));
+}
+
+function addIfAbsent(list, entry) {
+  if (!hasEntryContaining(list, entry)) {
+    list.push(entry);
+  }
+}
+
 module.exports = () => (neutrino) => {
   applyNeutrinoPatches(neutrino);
 
   neutrino.addSupportedExtensions('ts', 'tsx');
   neutrino.tapAtEnd('compile', 'babel', (options) => {
-    options.presets.push(['@babel/preset-typescript', {}]);
-    options.plugins.push(['@babel/plugin-proposal-class-properties', {}]);
-    options.plugins.push(['@babel/plugin-proposal-object-rest-spread', {}]);
+    addIfAbsent(options.presets, ['@babel/preset-typescript', {}]);
+    addIfAbsent(options.plugins, ['@babel/plugin-proposal-class-properties', {}]);
+    addIfAbsent(options.plugins, ['@babel/plugin-proposal-object-rest-spread', {}]);
     return options;
   });
 };
