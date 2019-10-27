5440a751152c4fe438405825075e8603c3684513
David Evans <davidje13@users.noreply.github.com>
2019-10-27 20:45:44 +0000

    Stop using outFile, since it causes 'X is not a module' errors

--- generateDeclaration.js
+++ generateDeclaration.js
@@ -1,27 +1,23 @@
-const path = require('path');
 const typescript = require('typescript');
 
 module.exports = function(neutrino, declarationMap) {
   const rawOptions = typescript.getDefaultCompilerOptions();
-  Object.values(neutrino.options.mains).forEach((entry) => {
-
+  const sources = Object.values(neutrino.options.mains).map((entry) => {
     if (typeof entry === 'object') {
-      entry = entry.entry;
+      return entry.entry;
     }
-
-    const outputName = `${path.parse(entry).name}.d.ts`;
-
-    const options = {
-      ...rawOptions,
-      noEmit: false, // this might not be needed any more
-      isolatedModules: false, // https://github.com/microsoft/TypeScript/issues/29490 (fixed in 3.6.4+)
-      declaration: true,
-      emitDeclarationOnly: true,
-      declarationMap,
-      declarationDir: neutrino.options.output, // not needed when using outFile
-      outFile: path.join(neutrino.options.output, outputName),
-    };
-
-    typescript.createProgram([entry], options).emit();
+    return entry;
   });
+
+  const options = {
+    ...rawOptions,
+    noEmit: false, // this might not be needed any more
+    isolatedModules: false, // https://github.com/microsoft/TypeScript/issues/29490 (fixed in 3.6.4+)
+    declaration: true,
+    emitDeclarationOnly: true,
+    declarationMap,
+    declarationDir: neutrino.options.output,
+  };
+
+  typescript.createProgram(sources, options).emit();
 }
