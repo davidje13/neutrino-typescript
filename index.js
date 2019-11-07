901cc58a19f467e4d4ecb15a7b217dbc113a0c8e
David Evans <davidje13@users.noreply.github.com>
2019-11-07 21:55:34 +0000

    Support TypeScript 3.7 syntax and make babel plugins configurable

--- index.js
+++ index.js
@@ -17,14 +17,21 @@ function addIfAbsent(list, entry) {
 module.exports = ({
   declaration = false,
   declarationMap = true,
+  looseProperties = false,
+  looseNullCheck = false,
 } = {}) => (neutrino) => {
   applyNeutrinoPatches(neutrino);
 
   neutrino.addSupportedExtensions('ts', 'tsx');
   neutrino.tapAtEnd('compile', 'babel', (options) => {
     addIfAbsent(options.presets, ['@babel/preset-typescript', {}]);
-    addIfAbsent(options.plugins, ['@babel/plugin-proposal-class-properties', {}]);
-    addIfAbsent(options.plugins, ['@babel/plugin-proposal-object-rest-spread', {}]);
+    addIfAbsent(options.plugins, ['@babel/plugin-proposal-class-properties', { loose: looseProperties }]);
+    addIfAbsent(options.plugins, ['@babel/plugin-proposal-nullish-coalescing-operator', { loose: looseNullCheck }]);
+    addIfAbsent(options.plugins, ['@babel/plugin-proposal-object-rest-spread', {
+      loose: looseProperties,
+      useBuiltIns: looseProperties,
+    }]);
+    addIfAbsent(options.plugins, ['@babel/plugin-proposal-optional-chaining', { loose: looseNullCheck }]);
     return options;
   });
 
